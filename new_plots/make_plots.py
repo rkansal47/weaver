d7f1afd9471ffcdc09257ee443067da149407436
rkansal47 <raghavk1997@yahoo.co.in>
2022-05-23 17:35:29 -0700

    new plots

---
+++ new_plots/make_plots.py
@@ -0,0 +1,211 @@
+"""
+Takes inferenced root files (output of weaver inference) and evaluates the tagger.
+
+Author(s): Raghav Kansal
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mplhep as hep
+import uproot
+import os
+import argparse
+
+plt.rcParams.update({"font.size": 16})
+plt.style.use(hep.style.CMS)
+hep.style.use("CMS")
+
+from plot_methods import (
+    create_branches,
+    reweight_qcd_pt,
+    make_cuts,
+    plot_hists,
+)
+
+
+# for local interactive testing
+args = type("test", (object,), {})()
+args.inferences_dir = "../../inferences/04_18_ak8_qcd_oneweight"
+args.plot_dir = "../../plots/05_23_testing"
+args.sample_files = ["qcd", "HHbbVV"]
+args.sample_names = ["QCD", "HHbbVV"]
+
+
+# from command line args
+parser = argparse.ArgumentParser()
+parser.add_argument("--inferences-dir", type=str, help="directory containing inferenced root files")
+parser.add_argument("--sample-files", nargs="+", type=str, help="root file names of samples")
+parser.add_argument("--sample-names", nargs="+", type=str, help="sample labels")
+parser.add_argument("--plot-dir", required=True, help="output dir")
+args = parser.parse_args()
+
+
+os.system(f"mkdir -p {args.plot_dir}")
+
+
+# new branches
+sum_scores = {
+    "qcdall": ["isQCDb", "isQCDbb", "isQCDc", "isQCDcc", "isQCDothers"],
+    "sigall": ["H_VV_4q_3q", "H_VV_4q_4q"],
+}
+
+T_scores = {
+    "4q_4q": ("H_VV_4q_4q", "qcdall"),
+    "4q_3q": ("H_VV_4q_3q", "qcdall"),
+    "HVV4q": ("sigall", "qcdall"),
+}
+
+# cuts
+pt_key = "pt"
+msd_key = "msoftdrop"
+
+cuts_list = [
+    {pt_key: [300, 1500], msd_key: [20, 320]},
+    {pt_key: [400, 600], msd_key: [60, 150]},
+    # {pt_key: [300, 1500], msd_key: [110, 140]},
+]
+
+var_labels = {pt_key: "pT", msd_key: "mSD"}
+
+# hist vars
+# "file tag": {"title": plot title, "score_label": scores to plot}
+hist_plot_vars = {
+    # "th4q": {
+    #     "title": "Non-MD Th4q",
+    #     "score_label": "fj_PN_H4qvsQCD",
+    # },
+    "thvv4q": {
+        "title": "MD THVV4q",
+        "score_label": "score_fj_THVV4q",
+    },
+}
+
+# roc vars
+roc_plot_vars = {
+    "th4q": {
+        "title": "Non-MD Th4q",
+        "score_label": "fj_PN_H4qvsQCD",
+        "colour": "orange",
+    },
+    "thvv4q": {
+        "title": "MD THVV4q",
+        "score_label": "score_fj_THVV4q",
+        "colour": "green",
+    },
+    "pnet4q": {
+        "title": "MD 4q only",
+        "score_label": "score_fj_TH_VV_4q_4q",
+        "colour": "red",
+        "sig_selector": "fj_H_VV_4q_4q",
+    },
+    "pnet3q": {
+        "title": "MD 3q only",
+        "score_label": "score_fj_TH_VV_4q_3q",
+        "colour": "blue",
+        "sig_selector": "fj_H_VV_4q_3q",
+    },
+}
+
+
+# create sample dict
+assert len(args.sample_files) == len(args.sample_names), "# sample files should = # sample names"
+samples = {args.sample_files[i]: args.sample_names[i] for i in range(len(args.sample_files))}
+
+# load samples
+events_dict = {
+    sample: uproot.open(f"{args.inferences_dir}/{sample}.root:Events").arrays()
+    for sample in samples
+}
+
+
+print("Creating new branches")
+create_branches(events_dict, sum_scores, T_scores)
+
+print("Reweighting QCD")
+reweight_qcd_pt(events_dict["qcd"])
+
+print("Making cuts")
+cuts_dict, cut_labels = make_cuts(events_dict, cuts_list, var_labels)
+
+for cutstr in cut_labels:
+    os.system(f"mkdir -p {args.plot_dir}/{cutstr}")
+
+print("Making histograms")
+plot_hists(hist_plot_vars, events_dict, cuts_dict, cut_labels, samples, args.plot_dir)
+
+
+##############################################
+# ROCs
+##############################################
+
+print("Making ROCs")
+
+
+from sklearn.metrics import roc_curve, auc
+
+rocs = {}
+sig_key = "HHbbVV"
+bg_key = "qcd"
+
+for cutstr in cut_labels:
+    rocs[cutstr] = {}
+
+    for t, pvars in roc_plot_vars.items():
+        sig_cut = cuts_dict[sig_key][cutstr]
+        bg_cut = cuts_dict[bg_key][cutstr]
+
+        if "sig_selector" in pvars:
+            sig_cut = sig_cut * events_dict[sig_key][pvars["sig_selector"]]
+
+        y_true = np.concatenate(
+            [
+                np.ones(np.sum(sig_cut)),
+                np.zeros(np.sum(bg_cut)),
+            ]
+        )
+
+        weights = np.concatenate(
+            (events_dict[sig_key]["weight"][sig_cut], events_dict[bg_key]["weight"][bg_cut])
+        )
+
+        score_label = pvars["score_label"]
+        scores = np.concatenate(
+            (
+                events_dict[sig_key][score_label][sig_cut],
+                events_dict[bg_key][score_label][bg_cut],
+            )
+        )
+        fpr, tpr, thresholds = roc_curve(y_true, scores, sample_weight=weights)
+        rocs[cutstr][t] = {"fpr": fpr, "tpr": tpr, "thresholds": thresholds, "auc": auc(fpr, tpr)}
+
+
+xlim = [0, 0.6]
+ylim = [1e-6, 1]
+
+for cutstr in cut_labels:
+    plt.figure(figsize=(12, 12))
+    for t, pvars in roc_plot_vars.items():
+        plt.plot(
+            rocs[cutstr][t]["tpr"],
+            rocs[cutstr][t]["fpr"],
+            label=f"{pvars['title']} AUC: {rocs[cutstr][t]['auc']:.2f}",
+            linewidth=2,
+            color=pvars["colour"],
+        )
+        plt.vlines(
+            x=rocs[cutstr][t]["tpr"][np.searchsorted(rocs[cutstr][t]["fpr"], 0.01)],
+            ymin=0,
+            ymax=0.01,
+            colors=pvars["colour"],
+            linestyles="dashed",
+        )
+    plt.hlines(y=0.01, xmin=0, xmax=1, colors="lightgrey", linestyles="dashed")
+    plt.yscale("log")
+    plt.xlabel("Signal Eff.")
+    plt.ylabel("BG Eff.")
+    plt.suptitle("HVV FatJet ROC Curves", y=0.95)
+    plt.title(cut_labels[cutstr], fontsize=20)
+    plt.xlim(*xlim)
+    plt.ylim(*ylim)
+    plt.legend()
+    plt.savefig(f"{args.plot_dir}/{cutstr}/roccurve_{cutstr}.pdf", bbox_inches="tight")
---
+++ new_plots/plot_methods.py
@@ -0,0 +1,185 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mplhep as hep
+import awkward as ak
+import pickle
+
+plt.rcParams.update({"font.size": 16})
+plt.style.use(hep.style.CMS)
+hep.style.use("CMS")
+
+from typing import Dict, List, Tuple
+
+
+def create_branches(
+    events_dict: Dict[str, ak.Array], sum_scores: Dict[str, List], T_scores: Dict[str, Tuple]
+):
+    """
+    Creates new branches in events in ``events_dict``.
+
+    ``sum_scores`` defines new branches which sum scores of existing branches.
+    ``T_scores`` defines new branches which = sig scores / (sig + bg scores).
+
+    Args:
+        events_dict (Dict[str, ak.Array]): {sample: awkward array of events}.
+        sum_scores (Dict[str, List]): {new branch label: [score branches to sum over]}.
+        T_scores (Dict[str, Tuple]): {new branch label: (sig, bg)}.
+
+    """
+
+    for sample, events in events_dict.items():
+        for label, subclasses in sum_scores.items():
+            events[f"score_fj_{label}"] = np.sum(
+                [events[f"score_fj_{subclass}"] for subclass in subclasses], axis=0
+            )
+
+        for label, (sig, bg) in T_scores.items():
+            events[f"score_fj_T{label}"] = events[f"score_fj_{sig}"] / (
+                events[f"score_fj_{sig}"] + events[f"score_fj_{bg}"]
+            )
+
+        events["weight"] = np.ones(len(events))
+
+
+def reweight_qcd_pt(events: ak.Array):
+    """Reweights QCD pT to be exponentially falling"""
+    from coffea.lookup_tools.dense_lookup import dense_lookup
+    from hist import Hist
+
+    reweight_bins = [120, 300, 1500]
+    bin_centers = np.linspace(reweight_bins[1], reweight_bins[2], reweight_bins[0] + 1)[:-1]
+    bin_centers += (bin_centers[1] - bin_centers[0]) / 2
+    # qcd pt 1/e range is roughly ~100 GeV
+    exp_falling_weights = np.exp((reweight_bins[1] - bin_centers) / 100)
+
+    qcd_pt = (Hist.new.Reg(*reweight_bins, name="pt").Double()).fill(
+        pt=events["fj_pt"],
+    )
+
+    pt_weights = exp_falling_weights / qcd_pt.view(flow=False)
+    pt_weights_lookup = dense_lookup(pt_weights, qcd_pt.axes.edges)
+
+    events["weight"] = pt_weights_lookup(events["fj_pt"])
+
+
+def make_cuts(events_dict: Dict[str, ak.Array], cuts_list: List[Dict], var_labels: Dict[str, str]):
+    """
+    Makes cuts on events in ``events_dict``.
+
+    ``cuts_list`` is a list of cut Dicts, each of format:
+    {
+        var1: [min, max],
+        var2: ...,
+    }
+
+    ``var_labels`` is a Dict of formatted labels for each cut var, for plotting.
+    {
+        var1: label,
+        var2: ...
+    }
+
+    Returns 1) a dictionary of cuts of format:
+    {
+        sample1: {
+            "cut1var1_min_max_cut1var2...": cut1,
+            "cut2var2...": cut2,
+            ...
+        },
+        sample2...
+    }
+
+    and 2) a dictionary of labels for plots of format:
+    {
+        "cut1var1_min_max_cut1var2...": "var1label: [min, max] var2label...",
+        "cut2var1...": ...
+    }
+
+    """
+    cuts_dict = {}
+    cut_labels = {}
+
+    for sample, events in events_dict.items():
+        cuts_dict[sample] = {}
+        for cutvars in cuts_list:
+            cutstrs = []
+            cutlabel = []
+            cuts = []
+            for cutvar, (cutmin, cutmax) in cutvars.items():
+                cutstrs.append(f"{cutvar}_{cutmin}_{cutmax}")
+                cutlabel.append(f"{var_labels[cutvar]}: [{cutmin}, {cutmax}]")
+                cuts.append(events[f"fj_{cutvar}"] >= cutmin)
+                cuts.append(events[f"fj_{cutvar}"] < cutmax)
+
+            cutstr = "_".join(cutstrs)
+            cut = np.prod(cuts, axis=0)
+            cuts_dict[sample][cutstr] = cut.astype(bool)
+
+            if cutstr not in cut_labels:
+                cut_labels[cutstr] = " ".join(cutlabel)
+
+    return cuts_dict, cut_labels
+
+
+def plot_hists(
+    hist_plot_vars: Dict[str, Dict],
+    events_dict: Dict[str, ak.Array],
+    cuts_dict: Dict[str, Dict],
+    cut_labels: Dict[str, str],
+    sample_labels: Dict[str, str],
+    plot_dir: str,
+    samples_colours: List[Tuple] = [("HHbbVV", "red"), ("qcd", "blue")],
+    save_hists: bool = False,
+):
+    """
+    Plots and (optionally) saves histograms.
+
+    ``hist_plot_vars`` is a dictionary of histograms to make formatted as:
+    {
+        'tag': {
+            'title': plot title,
+            'score_label': scores to make histograms of,
+        }
+    }
+    """
+
+    hists = {}
+
+    for t, pvars in hist_plot_vars.items():
+        hists[t] = {}
+        for cutstr in cut_labels:
+            hists[t][cutstr] = {}
+            plt.figure(figsize=(16, 12))
+            plt.suptitle(f"FatJet {pvars['title']} Scores", y=0.95)
+            plt.title(cut_labels[cutstr], fontsize=20)
+
+            for sample, colour in samples_colours:
+                _ = plt.hist(
+                    events_dict[sample][pvars["score_label"]][cuts_dict[sample][cutstr]],
+                    histtype="step",
+                    bins=np.linspace(0, 1, 101),
+                    label=f"{sample_labels[sample]}",
+                    linewidth=2,
+                    color=colour,
+                    density=True,
+                    weights=events_dict[sample]["weight"][cuts_dict[sample][cutstr]],
+                )
+
+                if save_hists:
+                    hists[t][cutstr][sample] = np.histogram(
+                        events_dict[sample][pvars["score_label"]][cuts_dict[sample][cutstr]],
+                        bins=np.linspace(0, 1, 101),
+                        weights=events_dict[sample]["weight"][cuts_dict[sample][cutstr]],
+                        density=True,
+                    )
+
+            plt.ylabel("Normalized # Jets")
+            plt.xlabel(f"PNet {pvars['title']} score")
+            plt.legend()
+            plt.savefig(
+                f"{plot_dir}/{cutstr}/{t}_hist_{cutstr}.pdf",
+                bbox_inches="tight",
+            )
+
+    if save_hists:
+        with open(f"{plot_dir}/hists.pkl", "wb") as f:
+            pickle.dump(hists, f)
